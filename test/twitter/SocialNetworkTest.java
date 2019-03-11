30a4ad01861a46c7441a0a889903319b4b4bff55
qaiser1996 <qaiser1996@users.noreply.github.com>
2019-03-11 17:09:37 +0500

    aster

--- test/twitter/SocialNetworkTest.java
+++ test/twitter/SocialNetworkTest.java
@@ -5,7 +5,9 @@ package twitter;
 
 import static org.junit.Assert.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +16,24 @@ import java.util.Set;
 import org.junit.Test;
 
 public class SocialNetworkTest {
+    
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+    private static final Instant d3 = Instant.parse("2016-02-17T11:25:00Z");
+    private static final Instant d4 = Instant.parse("2016-02-17T11:45:00Z");
+    private static final Instant d5 = Instant.parse("2016-02-17T11:47:00Z");
+    private static final Instant d6 = Instant.parse("2016-02-17T10:47:00Z");
+
+    private static final Tweet tweet1 = new Tweet(1, "xyz", "is it ok to talk about religion so much?", d1);
+    private static final Tweet tweet2 = new Tweet(2, "jkl", "hello this is lab 5 of SC", d2);
+    private static final Tweet tweet3 = new Tweet(3, "abc", "can we please go home?", d3);
+    private static final Tweet tweet4 = new Tweet(4, "abc", "@qaiser is this another user?",
+            d4);
+    private static final Tweet tweet5 = new Tweet(5, "abc2", "@qaiser @alishba hello how are you?",
+            d5);
+    private static final Tweet tweet6 = new Tweet(6, "abc2", "@qaiser @muneeb hello how good are you?",
+            d6);
+
 
     /*
      * TODO: your testing strategies for these methods should go here.
@@ -25,21 +45,53 @@ public class SocialNetworkTest {
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
+       
     
     @Test
     public void testGuessFollowsGraphEmpty() {
-        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
         
         assertTrue("expected empty graph", followsGraph.isEmpty());
     }
     
     @Test
     public void testInfluencersEmpty() {
-        Map<String, Set<String>> followsGraph = new HashMap<>();
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
         List<String> influencers = SocialNetwork.influencers(followsGraph);
         
         assertTrue("expected empty list", influencers.isEmpty());
     }
+    
+    @Test
+    public void testGuessFollowsGraphUsernameDifferentCases(){
+        Map<String, Set<String>> followsGraph = new HashMap<>();
+        SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4, tweet5, tweet6));
+        assertTrue(followsGraph.get("alishba").contains("qaiser"));
+    }
+    
+    @Test
+    public void testInfluencersSixTweets(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1,tweet2,tweet3,tweet4,tweet5,tweet6));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"qaiser");
+                  
+        }
+
+    @Test
+    public void testInfluencersSingleTweetnoInfluence(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"qaiser");
+                  
+        }
+
+    @Test
+    public void testInfluencersSingleTweetOneInfluence(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"qaiser");
+                  
+        }
 
     /*
      * Warning: all the tests you write here must be runnable against any
